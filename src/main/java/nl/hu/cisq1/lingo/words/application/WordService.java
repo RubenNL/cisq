c9c1fa2b5bd72ae6cfc3a25fc6a420a3c1009fe0
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-02-10 16:40:23 +0100

    Words inladen van bestand

--- src/main/java/nl/hu/cisq1/lingo/words/application/WordService.java
+++ src/main/java/nl/hu/cisq1/lingo/words/application/WordService.java
@@ -1,24 +1,36 @@
 package nl.hu.cisq1.lingo.words.application;
 
-import nl.hu.cisq1.lingo.words.data.SpringWordRepository;
 import nl.hu.cisq1.lingo.words.domain.exception.WordLengthNotSupportedException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @Transactional
 public class WordService {
-    private final SpringWordRepository wordRepository;
-
-    public WordService(SpringWordRepository wordRepository) {
-        this.wordRepository = wordRepository;
-    }
-
     public String provideRandomWord(Integer length) {
-        return this.wordRepository
-                .findRandomWordByLength(length)
-                .orElseThrow(() -> new WordLengthNotSupportedException(length))
-                .getValue();
+        List<String> items;
+        try {
+            URL url=getClass().getClassLoader().getResource(length + ".txt");
+            URI uri=url.toURI();
+            System.out.println(uri);
+            Path path=Paths.get(uri);
+            System.out.println(path);
+            items=Files.readAllLines(path);
+            System.out.println(items);
+        } catch(IOException | URISyntaxException | NullPointerException e) {
+            System.out.println(e);
+            throw new WordLengthNotSupportedException(length);
+        }
+        return items.get(new Random().nextInt(items.size()));
     }
 }
