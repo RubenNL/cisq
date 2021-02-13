62de430cc150b155f37b4f2625478b7f4cc688c8
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-02-13 18:28:57 +0100

    Feedback tests combined into ParameterizedTests.

--- src/test/java/nl/hu/cisq1/lingo/words/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/words/domain/FeedbackTest.java
@@ -3,35 +3,41 @@ package nl.hu.cisq1.lingo.words.domain;
 import nl.hu.cisq1.lingo.words.domain.exception.InvalidFeedbackException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedbackTest {
-	@Test
-	@DisplayName("word is guessed if all letters are correct")
-	void wordIsGuessed() {
-		Feedback feedback=new Feedback("woord", List.of(Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
-		assertTrue(feedback.isWordGuessed());
+	private static Stream<Arguments> provideGuessedTests() {
+		return Stream.of(
+				Arguments.of("woord", List.of(Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT),true),
+				Arguments.of("woord", List.of(Mark.PRESENT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT),false)
+		);
 	}
-	@Test
-	@DisplayName("word is not guessed if not all letters are correct")
-	void wordIsNotGuessed() {
-		Feedback feedback=new Feedback("woord", List.of(Mark.PRESENT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
-		assertFalse(feedback.isWordGuessed());
+	@ParameterizedTest
+	@DisplayName("word is guessed multi-test")
+	@MethodSource("provideGuessedTests")
+	void giveHintTest(String attempt, List marks,Boolean expected) {
+		Feedback feedback=new Feedback(attempt,marks);
+		assertEquals(expected,feedback.isWordGuessed());
 	}
-	@Test
-	@DisplayName("word is valid if all letters are valid")
-	void guessIsValid() {
-		Feedback feedback=new Feedback("woord", List.of(Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
-		assertTrue(feedback.isWordValid());
+	private static Stream<Arguments> provideValidTests() {
+		return Stream.of(
+				Arguments.of("woord", List.of(Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT),true),
+				Arguments.of("woord", List.of(Mark.INVALID,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT),false)
+		);
 	}
-	@Test
-	@DisplayName("word is invalid when 1 char is invalid.")
-	void guessIsInvalid() {
-		Feedback feedback=new Feedback("woord", List.of(Mark.INVALID,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT,Mark.CORRECT));
-		assertFalse(feedback.isWordValid());
+	@ParameterizedTest
+	@DisplayName("word is valid multi-test")
+	@MethodSource("provideValidTests")
+	void validTests(String attempt, List marks,Boolean expected) {
+		Feedback feedback=new Feedback(attempt,marks);
+		assertEquals(expected,feedback.isWordValid());
 	}
 	@Test
 	@DisplayName("invalid mark size test")
