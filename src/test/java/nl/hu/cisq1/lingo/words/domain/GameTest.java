fd4cf9c509c6f6dc92ad00cf12b8de3cd9695e3e
Ruben <15885455+RubenNL@users.noreply.github.com>
2021-02-27 15:11:54 +0100

    rewrite tests to use @BeforeEach function.

--- src/test/java/nl/hu/cisq1/lingo/words/domain/GameTest.java
+++ src/test/java/nl/hu/cisq1/lingo/words/domain/GameTest.java
@@ -1,22 +1,26 @@
 package nl.hu.cisq1.lingo.words.domain;
 
 import nl.hu.cisq1.lingo.words.domain.exception.IllegalActionException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+	Game game;
+	@BeforeEach
+	void beforeEach() {
+		game=new Game();
+	}
 	@Test
 	@DisplayName("startNewRound empty Test")
 	void startNewRoundEmpty() {
-		Game game=new Game();
 		assertDoesNotThrow(()->game.startNewRound(new Word("test")));
 	}
 	@Test
 	@DisplayName("startNewRound playing Test")
 	void startNewRoundStartedRound() {
-		Game game=new Game();
 		game.startNewRound(new Word("test"));
 		Word word=new Word("test");
 		assertThrows(IllegalActionException.class, () -> game.startNewRound(word));
@@ -24,7 +28,6 @@ class GameTest {
 	@Test
 	@DisplayName("startNewRound win before test")
 	void startNewRoundWinTest() {
-		Game game=new Game();
 		game.startNewRound(new Word("test"));
 		Round round=game.getLastRound();
 		round.addFeedback("test");
@@ -33,7 +36,6 @@ class GameTest {
 	@Test
 	@DisplayName("startNewRound lost before test")
 	void startNewRoundLostTest() {
-		Game game = new Game();
 		game.startNewRound(new Word("test"));
 		Round round = game.getLastRound();
 		round.addFeedback("abcd");
@@ -47,20 +49,17 @@ class GameTest {
 	@Test
 	@DisplayName("emptyScore test")
 	void emptyScoreTest() {
-		Game game = new Game();
 		assertEquals(0,game.getScore());
 	}
 	@Test
 	@DisplayName("single empty round score test")
 	void emptyRoundScoreTest() {
-		Game game = new Game();
 		game.startNewRound(new Word("abcd"));
 		assertEquals(0,game.getScore());
 	}
 	@Test
 	@DisplayName("single round score test")
 	void singleRoundScoreTest() {
-		Game game = new Game();
 		game.startNewRound(new Word("abcd"));
 		Round round=game.getLastRound();
 		round.addFeedback("abcd");
@@ -69,7 +68,6 @@ class GameTest {
 	@Test
 	@DisplayName("two rounds score test")
 	void twoRoundsScoreTest() {
-		Game game = new Game();
 		game.startNewRound(new Word("abcd"));
 		Round round = game.getLastRound();
 		round.addFeedback("abcd");
--- src/test/java/nl/hu/cisq1/lingo/words/domain/RoundTest.java
+++ src/test/java/nl/hu/cisq1/lingo/words/domain/RoundTest.java
@@ -1,6 +1,7 @@
 package nl.hu.cisq1.lingo.words.domain;
 
 import nl.hu.cisq1.lingo.words.domain.exception.IllegalActionException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoundTest {
+	Round round;
+	@BeforeEach
+	void beforeEach() {
+		round=new Round(new Word("abcdef"));
+	}
 	//Veel van deze tests zouden beter met ParameterizedTest gedaan kunnen worden, maar ik kreeg dat niet netjes.
 	@Test
 	@DisplayName("addFeedback simple test")
 	void addFeedback() {
-		Round round=new Round(new Word("abcdef"));
 		Feedback feedback=round.addFeedback("testab");
 		Feedback expected=new Feedback("testab",Feedback.generateMarks("testab","abcdef"));
 		assertEquals(expected,feedback,"Feedback is correct gereturned");
@@ -22,7 +27,6 @@ class RoundTest {
 	@Test
 	@DisplayName("addFeedback max test")
 	void addFeedbackMaxTest() {
-		Round round=new Round(new Word("abcdef"));
 		for(int i=0;i<Round.MAXROUNDS;i++) {
 			assertDoesNotThrow(() -> round.addFeedback("testab"), "first to max does not throw");
 		}
@@ -33,54 +37,46 @@ class RoundTest {
 	@Test
 	@DisplayName("prevent adding feedback when correct before")
 	void addFeedbackAfterCorrect() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdef");
 		assertThrows(IllegalActionException.class,()->round.addFeedback("abcdef"));
 	}
 	@Test
 	@DisplayName("wordGuessed empty test")
 	void wordGuessedEmptyTest() {
-		Round round=new Round(new Word("abcdef"));
 		assertFalse(round.wordGuessed(),"word should not be guessed when empty");
 	}
 	@Test
 	@DisplayName("wordGuessed incorrect 1 test")
 	void wordGuessedInvalidTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdeg");
 		assertFalse(round.wordGuessed(),"word should not be guessed with 1 incorrect guess");
 	}
 	@Test
 	@DisplayName("wordGuessed correct 1 test")
 	void wordGuessedCorrectTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdef");
 		assertTrue(round.wordGuessed(),"word should be guessed with 1 correct guess");
 	}
 	@Test
 	@DisplayName("score empty test")
 	void scoreEmptyTest() {
-		Round round=new Round(new Word("abcdef"));
 		assertEquals(0,round.getScore());
 	}
 	@Test
 	@DisplayName("score 1 failed test")
 	void score1failedTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdeg");
 		assertEquals(0,round.getScore());
 	}
 	@Test
 	@DisplayName("score 1 correct test")
 	void score1correctTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdef");
 		assertEquals(25,round.getScore());
 	}
 	@Test
 	@DisplayName("score 1 failed 1 correct test")
 	void score1failed1correctTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdeg");
 		round.addFeedback("abcdef");
 		assertEquals(20,round.getScore());
@@ -88,7 +84,6 @@ class RoundTest {
 	@Test
 	@DisplayName("score 5 failed test")
 	void score5failedTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdea");
 		round.addFeedback("abcdeb");
 		round.addFeedback("abcdec");
@@ -99,7 +94,6 @@ class RoundTest {
 	@Test
 	@DisplayName("score 4 failed 1 correct test")
 	void score4failed1correctTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("abcdea");
 		round.addFeedback("abcdeb");
 		round.addFeedback("abcdec");
@@ -110,37 +104,31 @@ class RoundTest {
 	@Test
 	@DisplayName("empty feedback test")
 	void emptyFeedbackTest() {
-		Round round=new Round(new Word("abcdef"));
 		assertEquals(List.of("a",".",".",".",".","."),round.giveHint());
 	}
 	@Test
 	@DisplayName("single feedback geen correct test")
 	void singleFeedbackNoCorrectTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("azzzzz");
 		assertEquals(List.of("a",".",".",".",".","."),round.giveHint());
 	}
 	@Test
 	@DisplayName("single feedback invalid")
 	void singleFeedbackInvalidTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("a");
 		assertEquals(List.of("a",".",".",".",".","."),round.giveHint());
 	}
 	@Test
 	@DisplayName("correctFeedback test")
 	void feedbackCorrectTest() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("afcdbe");
 		assertEquals(List.of("a",".","c","d",".","."),round.giveHint());
 	}
 	@Test
 	@DisplayName("1xinvalid 1xvalid test")
 	void feedbackvalidinvalid() {
-		Round round=new Round(new Word("abcdef"));
 		round.addFeedback("a");
 		round.addFeedback("acbedf");
 		assertEquals(List.of("a",".",".",".",".","f"),round.giveHint());
 	}
-
 }
